360aebb614bd9d4802dc76e155e169868d94b977
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-02-28 10:57:01 +0800

    Update wordpress_create.sh

--- wordpress_create.sh
+++ wordpress_create.sh
@@ -46,17 +46,17 @@ echo "$compose_content" > "$folder_name/docker-compose.yaml"
 echo "文件夹 '$folder_name' 和 compose.yaml 文件创建成功！"
 
 # 輸出 run.sh 腳本內容
-echo "#!/bin/bash" > "$project_name/run.sh"
-echo "" >> "$project_name/run.sh"
-echo "# 切換到項目目錄" >> "$project_name/run.sh"
-echo "cd \"\$(dirname \"\$0\")\"" >> "$project_name/run.sh"
-echo "" >> "$project_name/run.sh"
-echo "# 執行 docker compose" >> "$project_name/run.sh"
-echo "docker-compose up -d" >> "$project_name/run.sh"
-echo "" >> "$project_name/run.sh"
-echo "echo \"WordPress 已啟動！請在瀏覽器中輸入 http://localhost:9000 進行訪問。\"" >> "$project_name/run.sh"
+echo "#!/bin/bash" > "$folder_name/run.sh"
+echo "" >> "$folder_name/run.sh"
+echo "# 切換到項目目錄" >> "$folder_name/run.sh"
+echo "cd \"\$(dirname \"\$0\")\"" >> "$folder_name/run.sh"
+echo "" >> "$folder_name/run.sh"
+#echo "# 執行 docker compose" >> "$folder_name/run.sh"
+#echo "docker-compose up -d" >> "$folder_name/run.sh"
+#echo "" >> "$folder_name/run.sh"
+#echo "echo \"WordPress 已啟動！請在瀏覽器中輸入 http://localhost:9000 進行訪問。\"" >> "$folder_name/run.sh"
 
 # 修改 run.sh 腳本權限
-chmod +x "$project_name/run.sh"
+chmod +x "$folder_name/run.sh"
 
-echo "run.sh 腳本已生成到 $project_name 目錄中。"
+echo "run.sh 腳本已生成到 $folder_name 目錄中。"
