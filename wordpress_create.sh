eee735548586229addb5dcb3bda455adc50757f6
stevenke1981 <49631723+stevenke1981@users.noreply.github.com>
2024-02-28 11:08:08 +0800

    Update wordpress_create.sh

--- wordpress_create.sh
+++ wordpress_create.sh
@@ -51,7 +51,7 @@ echo "# 切換到項目目錄" >> "$folder_name/run.sh"
 echo "cd \"\$(dirname \"\$0\")\"" >> "$folder_name/run.sh"
 echo "" >> "$folder_name/run.sh"
 echo "# 執行 docker compose" >> "$folder_name/run.sh"
-echo "docker-compose up -d" >> "$folder_name/run.sh"
+echo "docker compose up -d" >> "$folder_name/run.sh"
 echo "" >> "$folder_name/run.sh"
 echo "echo \"WordPress 已啟動！請在瀏覽器中輸入 http://localhost:9000 進行訪問。\"" >> "$folder_name/run.sh"
 
